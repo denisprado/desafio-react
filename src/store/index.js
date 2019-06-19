8f782482cc26e93d92b21ca7c38d7f43fc643a73
denisprado <denisforigo@gmail.com>
2019-06-19 17:05:52 -0300

    defining router

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { routerMiddleware } from 'connected-react-router'
 
-import rootReducers from './ducks';
+import history from '../routes/history'
+import rootReducer from './ducks';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [sagaMiddleware];
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
 
-const store = createStore(rootReducers, applyMiddleware(...middlewares));
+const store = createStore(rootReducer(history)(rootReducer), applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga);
 
